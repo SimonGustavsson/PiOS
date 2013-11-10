d424233575100d3620104d41173fbf6e96007353
Simon Gustavsson <simongustavsson@driveworks.co.uk>
2013-11-10 02:18:05 +0000

    Added arg check to deploy script

--- BuildAndDeploy.bat
+++ BuildAndDeploy.bat
@@ -1,5 +1,8 @@
 @echo off
 
+if "%1"==""	GOTO INVALIDARGUMENTS
+if "%2"==""	GOTO INVALIDARGUMENTS
+
 SET DEPLOYERPATH=%1
 SET KERNELPATH=%2
 
@@ -14,13 +17,16 @@ SET KERNELPATH=%CD%
 popd
 
 REM Build
+echo Building...
 c:/cygwin64/bin/bash.exe --login -c "cd f:;cd git/pios;make"
 
-echo Deployer path: %DEPLOYERPATH%\PiOSDeployer.exe
-echo Kernel path: %KERNELPATH%\kernel.img
-
-REM And launch the deployer
-REM %DEPLOYERPATH%\PiOSDeployer.exe %KERNELPATH%kernel.img
-
+echo Deploying kernel...
 start "Deployer" %DEPLOYERPATH%\PiOSDeployer.exe %KERNELPATH%\kernel.img
 echo Waiting for Deployer...
+
+GOTO DONE
+
+:INVALIDARGUMENTS
+echo Invalid arguments to deploy script, have you set up Visual Studio correctly?
+
+:DONE
