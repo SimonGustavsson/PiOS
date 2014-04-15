aa0d170f2f3fd622f60269507a01d7ca0bb27fef
Simon Gustasson <simongustavsson@driveworks.co.uk>
2014-04-15 19:09:27 +0100

    Dummy1 app now loaded at a specific memory address, passing arg to swi

--- user/dummy1/source/main.c
+++ user/dummy1/source/main.c
@@ -1,15 +1,32 @@
-#define SVC_INSTRUCTION(number) asm volatile("svc %0" : : "I" (number))
-
 //
 // Example application Zero
 // Test to make sure loaded relocatable binaries can call SVC
 // Functions repeatedly and return as expected
 //
+#define SWI_PARAM_WITH_RES(SWI_NR, ARG, RESULT) \
+    asm volatile(\
+    "mov r0,%1  \t\n" \
+    "swi %a2     \n\t" \
+    "mov %0,r0  \n\t" \
+    : "=r" (RESULT) : "r" (ARG), "I" (SWI_NR) : "r0", "lr")
+	
+#define SYS_PRINT_SWI 12
+	
+// Forward declare
+void sys_print(const char* str);
+
 int main(void)
 {
-	unsigned int i;
-	for(i = 0; i < 4; i++)
-		SVC_INSTRUCTION(12);
+	sys_print("Hello, user program!");
 	
-	return 0;
+	return 4;
 }
+
+void sys_print(const char* str)
+{
+	// Note: res is not actually used at all
+	unsigned int res;
+	SWI_PARAM_WITH_RES(SYS_PRINT_SWI, str, res);
+}
+
+
