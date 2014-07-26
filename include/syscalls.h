8e442f594da0f5639f09986bef46d970ed6dd328
Simon Gustavsson <simongustavsson@driveworks.co.uk>
2014-07-26 20:43:07 +0100

    Test syscalls that will eventually move to user library

--- include/syscalls.h
+++ include/syscalls.h
@@ -1,1 +1,17 @@
+#include "thread.h"
+
+#define SWI_PARAM_WITH_RES(SWI_NR, ARG, RESULT) \
+    asm volatile(\
+    "mov r0,%1  \t\n" \
+    "swi %a2     \n\t" \
+    "mov %0,r0  \n\t" \
+    : "=r" (RESULT) : "r" (ARG), "I" (SWI_NR) : "r0", "lr")
+
+#define SYS_PRINT_SWI 12
+#define SYS_THREAD_CREATE 42
+
+// Invokes the system call to print a string
+void pios_print(const char* str); 
+thread* pios_thread_create(thread_entry entry);
+
 void syscalls_init(void);
--- source/syscalls.c
+++ source/syscalls.c
@@ -2,6 +2,24 @@
 #include "syscalls.h"
 #include "types/string.h"
 #include "stddef.h"
+#include "scheduler.h"
+#include "hardware/uart.h"
+
+void pios_print(const char* str)
+{
+    // Note: res is not actually used at all
+    unsigned int res;
+    SWI_PARAM_WITH_RES(SYS_PRINT_SWI, str, res);
+}
+
+thread* pios_thread_create(thread_entry entry)
+{
+    Uart_SendString("pios_create_thread called\n");
+    unsigned int res = NULL;
+    SWI_PARAM_WITH_RES(SYS_THREAD_CREATE, entry, res);
+
+    return (thread*)res;
+}
 
 void* sys_printf(int arg1, int arg2, int arg3)
 {
@@ -26,9 +44,16 @@ void* sys_dummy2(int arg1, int arg2, int arg3)
     return NULL;
 }
 
+thread* sys_create_thread(int arg1, int arg2, int arg3)
+{
+    printf("sys_create_thread called from SWI\n");
+    return thread_create((thread_entry)arg1);
+}
+
 void syscalls_init(void)
 {
     swi_install(12, sys_printf);
+    swi_install(SYS_THREAD_CREATE, sys_create_thread);
     swi_install(95, sys_dummy1);
     swi_install(96, sys_dummy2);
 }
