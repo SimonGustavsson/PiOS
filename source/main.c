eac493f4f707fde8ffff4d6c8b06f9026aa6d0d7
Simon Gustavsson <simongustavsson@driveworks.co.uk>
2014-08-28 22:24:26 +0100

    Test thread procs now make use of \n, removed TODO as I've figured it out.

--- source/main.c
+++ source/main.c
@@ -19,58 +19,67 @@ volatile unsigned int dataVarForPadding = 42;
 void foo3()
 {
     unsigned int i;
+    unsigned int num = 0;
+    char buf[20];
     while (1)
     {
-        Uart_SendString("Leep!\n");
+        my_sscanf(&buf[0], "\rFoo3(): %d", num);
+        Uart_SendString(buf);
+        num++;
+
+        for(int k = 0; k < 20; k++)
+            buf[k] = 0;
 
         for (i = 0; i < 30000000; i++);
 
         volatile unsigned int asd = *(int*)(0x1000 + 4092);
         volatile unsigned int sff = *(int*)(0x2000 + 4092);
         volatile unsigned int fasa = *(int*)(0x3000 + 4092);
-        printf("Values: 0x%h, 0x%h, 0x%h\n", asd, sff, fasa);
+      //  printf("Values: 0x%h, 0x%h, 0x%h\n", asd, sff, fasa);
     }
 }
 
 void foo2()
 {
-    *(int*)(0x1000 + 4092) = 4;
-    *(int*)(0x2000 + 4092) = 5;
-    *(int*)(0x3000 + 4092) = 6;
-
-    unsigned int va = KERNEL_VA_START + Scheduler_GetCurrentThread()->owner->mem_pages[0];
-    printf("0x%h = 0x%h\n", va, *(unsigned int*)va);
-
     unsigned int i;
+    unsigned int num = 0;
+    char buf[20];
     while (1)
     {
-        Uart_SendString("foo2()\n");
+        my_sscanf(&buf[0], "\rFoo2(): %d", num);
+        Uart_SendString(buf);
+        num++;
+
+        for(int k = 0; k < 20; k++)
+            buf[k] = 0;
 
         for (i = 0; i < 30000000; i++);
 
         volatile unsigned int asd = *(int*)(0x1000 + 4092);
         volatile unsigned int sff = *(int*)(0x2000 + 4092);
         volatile unsigned int fasa = *(int*)(0x3000 + 4092);
-        printf("Values: 0x%h, 0x%h, 0x%h\n", asd, sff, fasa);
+    //    printf("Values: 0x%h, 0x%h, 0x%h\n", asd, sff, fasa);
     }
 }
 
 void foo()
 {
-    Uart_SendString("Inside foo() Testing page access\n");
     //thread* child = pios_thread_create((thread_entry)&foo3);
-
-     *(int*)(0x1000 + 4092) = 1;
-     *(int*)(0x2000 + 4092) = 2;
-     *(int*)(0x3000 + 4092) = 3;
 
     //unsigned int va = KERNEL_VA_START + Scheduler_GetCurrentThread()->owner->mem_pages[0];
     //printf("0x%h = 0x%h\n", va, *(unsigned int*)va);
 
     unsigned int i;
+    unsigned int num = 0;
+    char buf[20];
     while (1)
     {
-        Uart_SendString("foo()\n");
+        my_sscanf(&buf[0], "\rFoo(): %d", num);
+        Uart_SendString(buf);
+        num++;
+
+        for(int k = 0; k < 20; k++)
+            buf[k] = 0;
      
         for (i = 0; i < 30000000; i++);
 
@@ -104,10 +113,13 @@ void test_ttb0(void)
     printf("Testing %d bytes (0x%h)\n", bytesToCheck, bytesToCheck);
     for (int i = 0; i < bytesToCheck; i++)
     {
-        volatile unsigned int k = *(int*)(0x1000 + i);
+        volatile int* k = *(int*)(0x1000 + i);
     }
 
     printf("Tested %d bytes (%d pages). No output = Good output\n", bytesToCheck, bytesToCheck / 4096);
+
+    printf("Reading bottom of stack...\n");
+    int stackbottom = *(int*)(THREAD_STACK_VA_START - 4);
 }
 
 int cmain(void)
@@ -123,14 +135,18 @@ int cmain(void)
     if (fooProcess == NULL)
         printf("Failed to create foo task!\n");
 
-   // Process* fooProcess2 = Process_Create((unsigned int)&foo2, "Foo2(Test)", true);
-    //if (fooProcess2 == NULL)
-     //   printf("Failed to create foo2 task!\n");
-        
+    Process* fooProcess2 = Process_Create((unsigned int)&foo2, "Foo2(Test)", true);
+    if (fooProcess2 == NULL)
+        printf("Failed to create foo2 task!\n" );
+    
+
+    Process* fooProcess3 = Process_Create((unsigned int)&foo3, "Foo3(Test)", true);
+    if (fooProcess3 == NULL)
+       printf("Failed to create foo3 task!\n");
+
     printf("Starting scheduler...\n");
     Scheduler_Start();
-    printf("\nNot sure what to do now...\n");
-
+    
     unsigned int i;
     while (1)
     {
